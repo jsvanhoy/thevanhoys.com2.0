c57d1d13bb37e59c49bb80b27d4e914760bbcb65
jordan.vanhoy <jordan.vanhoy@bisonline.com>
2020-03-08 22:08:38 -0400

    adding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <v-app>
     <v-content>
-      <v-app-bar
-        dark
-        fixed
-        >
+      <!-- <v-img class="background-img" src="https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_1280.jpg"/> -->
+      <v-app-bar dark collapse-on-scroll fixed >
         <a style="color:white;" @click="$router.push({name: 'Home'})">
           <v-icon style="color:white">mdi-home</v-icon>
         </a>
@@ -31,8 +29,22 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+  body{
+    margin:0;
+  }
   .main{
     padding-top:55px;
+  }
+  .v-responsive.background-img{
+    position: fixed;
+    top:0px;
+    height:100vh;
+    width:100vw;
+    opacity: 0.4;
+    z-index: 0;
+  }
+  .v-application{
+    min-height: 0px !important;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="home">
-    <v-img
-      height="'100%'"
-      src="https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_1280.jpg"></v-img>
-
-    
+    <v-card class="center" elevation="24">
+      <v-parallax height="700" src="@/assets/img/main_pic.jpg"/>
       
+      <h1>Jordan</h1>
+      <p>
+        I currently work for Business Information Systems (BIS) in Piny Flats doing web development.
+         My hobbies include power sports, video games, and outdoor activities.
+         My current skill-set includes VueJS, Webpack, php, JS, Java, SQL, sass, babel, and Node.js.
+         The site you are now seeing was made with VueJS + TypeScript with Vuetify as its front end framework.
+      </p>
+      <v-parallax height="400" src="https://cdn.pixabay.com/photo/2016/10/18/21/22/california-1751455_1280.jpg"/>
+      <h1>Megan</h1>
+      <p>
+        I currently work at Sullivan East Middle School as a 7th grade Social Studies and Criminal Justice teacher.  
+        I also coach softball and volleyball.
+        My hobbies include traveling, reading, and outdoor activities.
+      </p>
+      <v-parallax height="400" src="https://cdn.pixabay.com/photo/2017/03/27/16/50/beach-2179624_1280.jpg"/>
+      <h1>Gibson</h1>
+      <p>Gibson is a mixed jack russel terrior.
+        He enjoys cuddling with his blanket, playing with toys, and eating treats. 
+      </p>
+      <v-img height="500" contain src="@/assets/img/gibson.jpg"/>
+    </v-card>
   </div>
 </template>
 
@@ -18,7 +36,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
->>> .v-parallax{
-  height:25%
-}
+  .v-card{
+    margin:50px;
+    padding:15px;
+    max-width: 1200px;
+    margin-left:auto;
+    margin-right:auto;
+  }
+  .center{
+    text-align: center;
+  }
 </style>
